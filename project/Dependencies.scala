87a6946a2b3a1657f088b257f2e6da5b9b26268c
jczuchnowski <jakub.czuchnowski@gmail.com>
2020-02-01 16:59:57 +0100

    fix zio-nio version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,6 +2,6 @@ import sbt._
 
 object Dependencies {
   lazy val zio    = "dev.zio" %% "zio"     % "1.0.0-RC17"
-  lazy val zioNio = "dev.zio" %% "zio-nio" % "0.4.0+10-c40ec920+20200114-0201"
+  lazy val zioNio = "dev.zio" %% "zio-nio" % "1.0.0-RC2"
   lazy val zioStreams    = "dev.zio" %% "zio-streams"     % "1.0.0-RC17"
 }
